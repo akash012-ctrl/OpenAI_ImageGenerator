aff7b6702467c8e7666fe135332c58b8e713d77e
akash012-ctrl <akashgo.ac@gmail.com>
2022-12-16 00:34:03 +0530

    openAI frontEnd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 const app = express();
 
@@ -9,8 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 
-
-
+//Set static folder
+app.use(express.static(path.join(__dirname,'public')));
 
 
 
